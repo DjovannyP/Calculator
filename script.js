c43ee7158086bc6991891c730f1b69888dc684d4
DjovannyP <pierredjovanny77@gmail.com>
2024-01-30 14:54:43 -0400

    Feat multiple function and events added

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
 
-const container = document.querySelector('.container');
-    let number1 = 0;
-let number2 = 0;
-let operator = '';
+//all numbers(0 to 9), equal, dot and the clear button include
+const numbers = document.querySelectorAll('.number');
+
+
+//Operators: "+", "-", "*", "/"
+const operators = document.querySelectorAll('.operator')
+//display
+const previousDisplay = document.querySelector('.previous');
+const currentDisplay = document.querySelector('.current');
+
+//tracking values 
+let previousValue = '';
+let currentValue = '' 
+let operator = ''
+
+//grab numbers and others elements like "=", "clear"
+for (let number of numbers) {
+    const value = number.value;
+    number.addEventListener('click', () => {
+        switch (value) {
+            case 'clear':
+                clear();
+                break;
+            case '.':
+                addDecimal();
+                break;
+            case '=':
+               const result = operate(operator, previousValue, currentValue);
+                currentDisplay.innerHTML = roundNumber(result);
+                break;
+            default:
+                if (currentValue.length < 5) {
+                    currentValue += value;
+                currentDisplay.innerHTML += value;
+                }
+                
+        }
+    })
+}
+
+//grab operators
+
+for (let element of operators) {
+    const symbol = element.value;
+    element.addEventListener('click', () => {
+        operator = symbol;
+        previousValue += currentValue;
+        currentValue = '';
+        previousDisplay.innerHTML = `${previousValue} ${symbol}`;
+        currentDisplay.innerHTML = currentValue;
+    })
+}
+
 
 function add(a, b) {
     return a + b;
@@ -19,22 +68,45 @@ function divide(a, b) {
     return a / b; 
 }
 
-function operate(operator, number1, number2) {
+function operate(operator,  previousValue,  currentValue) {
+
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
     switch (operator) {
         case '+':
-            return add(number1, number2);
+            return add(previousValue,  currentValue);
         case '-':
-            return subs(number1, number2);
+            return subs(previousValue,  currentValue);
         case '*':
-            return multiply(number1, number2);
+            return multiply(previousValue,  currentValue);
         case '/':
-            return divide(number1, number2);
+            return divide(previousValue,  currentValue);
         default:
             return '';
     }
 }
 
 
-for(let i = 0; i < 4; i++){
-    
+const roundNumber = number => Math.round(number * 1000) / 1000;
+
+//clear everything
+const clear = () => {
+    currentValue = '';
+    previousValue = '';
+    operator = '';
+    previousDisplay.innerHTML = '';
+    currentDisplay.innerHTML = '';
+}
+ 
+//decimal function
+const addDecimal = () => {
+    if (!currentValue.includes(".")) {
+        if (currentValue === '' || currentValue === '0' || operator !== '') {
+            currentValue = '0.';
+        } else {
+            currentValue += '.';
+        }
+        currentDisplay.innerHTML = currentValue;
+    }
 }
